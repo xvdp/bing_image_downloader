f045611ce1b86d0b6de9efc4dbdb246a50d32d52
xvdp <xvdpahlen@gmail.com>
2023-05-18 18:56:07 -0700

    added size, color, img_type, aspect filters

--- bing_image_downloader/bing.py
+++ bing_image_downloader/bing.py
@@ -1,3 +1,5 @@
+import warnings
+from typing import Optional
 from pathlib import Path
 import urllib.request
 import urllib
@@ -12,12 +14,34 @@ Author: Guru Prasad (g.gaurav541@gmail.com)
 
 
 class Bing:
-    def __init__(self, query, limit, output_dir, adult, timeout,  filter='', verbose=True):
+    def __init__(self,
+                 query: str,
+                 limit: int,
+                 output_dir: str,
+                 adult: str,
+                 timeout: int,
+                 img_type: Optional[str] = None, # filters
+                 color: Optional[str] = None, # filters
+                 size: Optional[str] = None,
+                 aspect: Optional[str] = None,
+                 verbose: bool = True) -> None:
+        """
+        added filter args
+        filter args:
+            color       None(all) or color, bw, RED, ORANGE, GREEN, YELLOW, TEAL, BLUE,
+                                        PURPLE, BROWN, BLACK, GRAY, WHITE
+                img_type, size and aspect can either be full name or first initial
+            img_type    None(all) or [l]inedrawing, [p]hoto, [c]lipart, [g]if | [a]nimatedgif, [t]ransparent
+            size        None(all) [w]allpaper, [l]arge, [m]edium, [s]mall
+            aspect      None(all) [s]quare [w]ide [t]all
+            
+
+        """
         self.download_count = 0
         self.query = query
         self.output_dir = output_dir
         self.adult = adult
-        self.filter = filter
+        self.filters = self.get_filters(img_type, color, size, aspect)
         self.verbose = verbose
         self.seen = set()
 
@@ -38,19 +62,39 @@ class Bing:
       'Connection': 'keep-alive'}
 
 
-    def get_filter(self, shorthand):
-            if shorthand == "line" or shorthand == "linedrawing":
-                return "+filterui:photo-linedrawing"
-            elif shorthand == "photo":
-                return "+filterui:photo-photo"
-            elif shorthand == "clipart":
-                return "+filterui:photo-clipart"
-            elif shorthand == "gif" or shorthand == "animatedgif":
-                return "+filterui:photo-animatedgif"
-            elif shorthand == "transparent":
-                return "+filterui:photo-transparent"
+    @staticmethod
+    def get_filters(img_type, color, size, aspect):
+        """ multiple image filters"""
+        out = ''
+        if img_type is not None:
+            _types = {'l':'linedrawing', 'p':'photo', 'g':'animatedgif', 'a':'animatedgif',
+                      't':'transparent'}
+            _t = img_type.lower()[0]
+            if _t in _types:
+                out += f"+filterui:photo-{_types[_t]}"
             else:
-                return ""
+                warnings.warn(f"ignoring, image type: {img_type} not found in {_types.values()}")
+        if color is not None:
+            _colors=("RED", "ORANGE", "YELLOW", "GREEN", "TEAL", "BUE", "PURPLE", "BROWN",
+                     "BLACK", "GRAY", "WHITE")
+            if color.upper() in _colors:
+                out += f"+filterui:color2-FGcls_{color.upper()}"
+            elif color.lower() in ("color", "bw"):
+                out += f"+filterui:color2-{color.lower()}"
+            else:
+                _colors = list(_colors) + ['color', 'bw']
+                warnings.warn(f"ignoring, image color: {color} not found in {_colors}")
+        if size is not None:
+            _sizes = {'w':'wallpaper', 'l':'large', 'm':'medium', 's':'small'}
+            if size.lower()[0] in _sizes:
+                out += f"+filterui:imagesize-{_sizes[size.lower()]}"
+            else:
+                warnings.warn(f"ignoring, image size: {size} not found in {_sizes.values()}")
+        if aspect is not None:
+            _aspects = {'s':'square', 'w':'wide', 't':'tall'}
+            if aspect.lower()[0] in _aspects:
+                out += f"+filterui:aspect-{_aspects[aspect.lower()]}"
+        return out
 
 
     def save_image(self, link, file_path):
@@ -62,7 +106,6 @@ class Bing:
         with open(str(file_path), 'wb') as f:
             f.write(image)
 
-    
     def download_image(self, link):
         self.download_count += 1
         # Get the image link
@@ -70,13 +113,14 @@ class Bing:
             path = urllib.parse.urlsplit(link).path
             filename = posixpath.basename(path).split('?')[0]
             file_type = filename.split(".")[-1]
-            if file_type.lower() not in ["jpe", "jpeg", "jfif", "exif", "tiff", "gif", "bmp", "png", "webp", "jpg"]:
+            if file_type.lower() not in ["jpe", "jpeg", "jfif", "exif", "tiff",
+                                         "gif", "bmp", "png", "webp", "jpg"]:
                 file_type = "jpg"
-                
+
             if self.verbose:
                 # Download the image
                 print("[%] Downloading Image #{} from {}".format(self.download_count, link))
-                
+
             self.save_image(link, self.output_dir.joinpath("Image_{}.{}".format(
                 str(self.download_count), file_type)))
             if self.verbose:
@@ -94,7 +138,7 @@ class Bing:
             # Parse the page source and download pics
             request_url = 'https://www.bing.com/images/async?q=' + urllib.parse.quote_plus(self.query) \
                           + '&first=' + str(self.page_counter) + '&count=' + str(self.limit) \
-                          + '&adlt=' + self.adult + '&qft=' + ('' if self.filter is None else self.get_filter(self.filter))
+                          + '&adlt=' + self.adult + '&qft=' + self.filters
             request = urllib.request.Request(request_url, None, headers=self.headers)
             response = urllib.request.urlopen(request)
             html = response.read().decode('utf8')
--- test.py
+++ test.py
@@ -3,10 +3,6 @@ from bing_image_downloader import downloader
 
 query=sys.argv[1]
 
-if len(sys.argv) == 3:
-    filter=sys.argv[2]
-else:
-    filter=""
     
             
 downloader.download(
@@ -16,7 +12,6 @@ downloader.download(
     adult_filter_off=True,
     force_replace=False,
     timeout=60,
-    filter=filter,
     verbose=True,
 )
 
